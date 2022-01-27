5b53502c0137e664112821f697e2ac2036a3d033
Maximilian <m.kens@tu-bs.de>
2022-01-27 19:24:55 +0100

    von katja in max gemergt

--- strahl.cpp
+++ strahl.cpp
@@ -5,12 +5,18 @@ Strahl::Strahl(){
     TVektor null(3);
     this->ursprung = null;
     this->richtung = null;
+    this->entfernung = null;
+    this->schnittpunkt = null;
+    this->normale = null;
 
 }
 
 Strahl::Strahl(TVektor ursprung, TVektor richtung){
 	this->ursprung = ursprung;
 	this->richtung = richtung;
+    this->entfernung = null;
+    this->schnittpunkt = null;
+    this->normale = null;
         
 }
 
